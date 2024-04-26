737b7b0ed3a9a8d93fee0bd353e524dfe3fa0205
salma <salmakchaw123@gmail.com>
2024-04-26 18:31:07 +0100

    changed error message for user creation

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -3,17 +3,17 @@ import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 export class CreateUserDto {
   @IsNotEmpty(
     {
-      message:"field $property  is compulsory"
+      message:"$property is compulsory"
     }
   )
   @IsEmail()
   email:string;
 
   @IsNotEmpty({
-    message:"field $property  is compulsory"
+    message:"$property is compulsory"
   })
   @MinLength(6, {
-      message: "the length of your  $property ($value) is inferior to the minimum length required ($constraint1)"
+      message: "! $property minimum length required ($constraint1)"
     }
   )
   password?:string;
