6c42a0851a8329236f33b678690cee8b175a2d8c
Younes Makhlouf <younes.makhlouf@insat.ucar.tn>
2024-05-25 23:30:06 +0100

    Properly sending leaderboard

--- src/quizz-session/quiz-session.service.ts
+++ src/quizz-session/quiz-session.service.ts
@@ -1,26 +1,22 @@
-import { Injectable } from '@nestjs/common';
-import { CreateQuizSessionDto } from './dto/create-quiz-session.dto';
-import { UpdateQuizSessionDto } from './dto/update-quiz-session.dto';
-import { v4 as uuidv4 } from 'uuid';
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {CreateQuizSessionDto} from './dto/create-quiz-session.dto';
+import {v4 as uuidv4} from 'uuid';
 import {QuizSession} from "./entities/quiz-session.entity";
-import {Quiz} from "../quizzes/entities/quiz.entity";
-import {User} from "../users/entities/user.entity";
-import {MessageBody} from "@nestjs/websockets";
 
 @Injectable()
 export class QuizSessionService {
-  quizzes : Map<any,any> =new Map();
-  createQuiz(quizDto: CreateQuizSessionDto,quizzes:Map<any,any>): string {
-    console.log("please please please");
-    const quizCode = uuidv4();
-    console.log(quizCode)
-    const {quiz,owner}=quizDto;
-    console.log(quiz);
-    const quizSession : QuizSession ={quiz :quiz , quizCode:quizCode,owner:owner ,hasStarted :false,players:[]}
-    this.quizzes.set(quizCode,quizSession);
-    quizzes.set(quizCode,quizSession);
-    return quizCode;
-  }
+    quizzes: Map<string, QuizSession> = new Map();
+
+    createQuiz(quizDto: CreateQuizSessionDto, quizzes: Map<any, any>): string {
+        const quizCode = uuidv4();
+        console.log(quizCode)
+        const {quiz, owner} = quizDto;
+        console.log(quiz);
+        const quizSession: QuizSession = {quiz: quiz, quizCode: quizCode, owner: owner, hasStarted: false, players: []}
+        this.quizzes.set(quizCode, quizSession);
+        quizzes.set(quizCode, quizSession);
+        return quizCode;
+    }
 
     joinQuiz(quizCode: string, playerId: string, playerName: string, quizzes: Map<string, QuizSession>): boolean {
         const quiz = quizzes.get(quizCode);
@@ -38,19 +34,64 @@ export class QuizSessionService {
             quizSession.hasStarted = true;
             return quiz.questions; // Send the questions to all players
         }
-        return [];
+        throw new NotFoundException(`Quiz session with code ${quizCode} not found`);
     }
+
+    processLeaderboard(quizCode: string) {
+        const quizSession = this.quizzes.get(quizCode);
+        if (quizSession) {
+            const leaderboard = quizSession.players
+                .sort((a, b) => b.score - a.score)
+                .map((player, index) => ({
+                    rank: index + 1, pseudo: player.pseudo, score: player.score,
+                }));
+            return leaderboard;
+        }
+    }
+
+    // processLeaderboard(quizCode: string) {
+    //     const leaderboard = [{
+    //         id: 1, name: "Eva Green", score: 225, avatar: "https://randomuser.me/api/portraits/women/24.jpg"
+    //     }, {id: 2, name: "James Smith", score: 200, avatar: "https://randomuser.me/api/portraits/men/24.jpg"}, {
+    //         id: 3, name: "Isabella Johnson", score: 250, avatar: "https://randomuser.me/api/portraits/women/25.jpg"
+    //     }, {
+    //         id: 4, name: "Ethan Williams", score: 195, avatar: "https://randomuser.me/api/portraits/men/25.jpg"
+    //     }, {
+    //         id: 5, name: "Sophia Brown", score: 180, avatar: "https://randomuser.me/api/portraits/women/26.jpg"
+    //     }, {
+    //         id: 6, name: "Daniel Davis", score: 210, avatar: "https://randomuser.me/api/portraits/men/26.jpg"
+    //     }, {
+    //         id: 7, name: "Olivia Garcia", score: 205, avatar: "https://randomuser.me/api/portraits/women/27.jpg"
+    //     }, {
+    //         id: 8, name: "Matthew Rodriguez", score: 175, avatar: "https://randomuser.me/api/portraits/men/27.jpg"
+    //     }, {
+    //         id: 9, name: "Ava Martinez", score: 230, avatar: "https://randomuser.me/api/portraits/women/28.jpg"
+    //     }, {
+    //         id: 10, name: "Michael Hernandez", score: 190, avatar: "https://randomuser.me/api/portraits/men/28.jpg"
+    //     }, {
+    //         id: 11, name: "Emily Gonzalez", score: 215, avatar: "https://randomuser.me/api/portraits/women/29.jpg"
+    //     }, {
+    //         id: 12, name: "Jacob Wilson", score: 165, avatar: "https://randomuser.me/api/portraits/men/29.jpg"
+    //     }, {
+    //         id: 13, name: "Mia Anderson", score: 220, avatar: "https://randomuser.me/api/portraits/women/30.jpg"
+    //     }, {
+    //         id: 14, name: "Alexander Thomas", score: 160, avatar: "https://randomuser.me/api/portraits/men/30.jpg"
+    //     }];
+    //     return leaderboard;
+    // }
 
     findAll(): Map<string, QuizSession> {
         return this.quizzes;
     }
 
-    findOne(@MessageBody() code: string, quizzes: Map<string, QuizSession>): QuizSession {
-        if (!quizzes[code]) return
-        return
+    findOne(code: string, quizzes: Map<string, QuizSession>): QuizSession {
+        if (!quizzes[code]) throw new NotFoundException(`Quiz session with code ${code} not found`);
+        return quizzes[code];
     }
 
     remove(code: string, quizzes: Map<string, QuizSession>) {
-        quizzes.delete(code);
+        if (!quizzes.delete(code)) {
+            throw new NotFoundException(`Quiz session with code ${code} not found`);
+        }
     }
 }
